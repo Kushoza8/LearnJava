8614297d4653f251d20d853d84e59af77777525e
Kushoza8 <115579530+Kushoza8@users.noreply.github.com>
2023-09-05 01:25:18 +0530

    Add files via upload
    JAVA 4TH SEPTEMBER

---
+++ Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+    }
+}
+
+//java is made up of function and classes.
+//void has no return value
+//static helps you run a function without calling them
+//use pascal convention in java in classes
+//camel case convention in functions
+// AddTwoNumbers-> pascal naming convention
+// addTwoNumbers-> Camel Casing convention
+//variable is a placeholder for a value and behaves exactly as the value it contains
+//byte data type is used when memory saving matters and is used in place of integer datatype
---
+++ One.java
@@ -0,0 +1,13 @@
+public class One {
+    public static void main(String[]args){
+        int x=2;
+        int y=2;
+        int z=4;
+        int w;
+        w=x+y+z;
+        System.out.println("the addition of 3 numbers "+x+" and "+y+" and "+z+" is "+w);
+    }
+}
+//println used to print in new line
+//print is used to print in the next line
+//println vs print do not forget !!!!
