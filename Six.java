3a842329c4a0f334d2e978753e91b0cda98f264d
Kushoza8 <115579530+Kushoza8@users.noreply.github.com>
2023-09-07 20:05:49 +0530

    Update Six.java

--- Six.java
+++ Six.java
@@ -1,6 +1,15 @@
 public class Six {
     public static void main(String[] args) {
-
+        char grade='B';
+        //to encrypt
+        grade=(char)(grade+8);
+        //the 5th line is known as typecasting
+        System.out.println(grade);
+        //character+integer is always an integer
+        //can be converted to char by typecasting
+        //to decrypt
+        grade=(char)(grade-8);
+        System.out.println(grade);
     }
 
 }
