d0a74bf7bfb0f1734baf29ba09cae9649025bafc
Kush Oza <ozakr@rknec.edu>
2023-09-24 13:22:18 +0530

    Add files via upload

---
+++ Private_Keyboard_Demo/Class.java
@@ -0,0 +1,10 @@
+package Protected_keyword_demo;
+public class Class {
+    protected int n;
+    String name;
+
+    public Class(int n, String name) {
+        this.n = n;
+        this.name = name;
+    }
+}
