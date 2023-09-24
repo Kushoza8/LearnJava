6054a2673a43167cc2a79cbfe125858ef669df62
Kush Oza <ozakr@rknec.edu>
2023-09-24 16:26:34 +0530

    Add files via upload

---
+++ Object_Demo.java
@@ -0,0 +1,27 @@
+public class Object_Demo {
+    //hashcode is unique representation of object via a number.
+    int num;
+
+    public Object_Demo(int num) {
+        this.num = num;
+    }
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
